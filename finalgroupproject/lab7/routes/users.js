16e4958a662939a333127db9693ad612758f4154
BettAD1 <adr.bett@gmail.com>
2024-11-10 23:13:05 -0500

    Updated Lab 10
    This is our final version of Lab 10

--- finalgroupproject/lab7/routes/users.js
+++ finalgroupproject/lab7/routes/users.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-//POST to create new user listing
+let userData = {}; 
 router.post('/create', (req, res) => {
-  const userData = req.body;
-  console.log('User Data Received:', userData);
-  res.json({ message: `User ${userData.username} was created successfully!`, data: userData });
+    userData = req.body; 
+    res.json({ message: `User ${userData.username} was created successfully!`, data: userData });
+});
+router.get('/profile', (req, res) => {
+    res.json(userData); 
 });
 module.exports = router;
